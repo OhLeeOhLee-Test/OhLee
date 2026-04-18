e9b83a6d2254799245f4ca15b184c48e6cb788a1
YeHwanLee <139995082+YeHwanLee@users.noreply.github.com>
2026-04-18 23:56:07 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header.jsx';
 import Home from './pages/home/Home.jsx';
+
+// ⭐️ 예환님이 알려주신 '진짜' 파일 위치로 수입(import) 경로를 고쳤습니다!
 import Punggyeong from './pages/projects/Punggyeong/Punggyeong.jsx';
 
 export default function App() {
   return (
-    // ⭐️ HashRouter가 깃허브 배포 시 주소 에러를 막아줍니다!
     <HashRouter>
-      {/* 헤더는 어느 방을 가든 항상 위에 떠 있어야 하므로 Routes 밖에 둡니다 */}
       <Header />
-
+      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/projects/Punggyeong/Punggyeong"
-          element={<Punggyeong />}
-        />
+        
+        {/* ⭐️ 목적지 주소: Home.jsx에서 navigate('/project/punggyeong') 한 곳이 바로 여기입니다! */}
+        <Route path="/project/punggyeong" element={<Punggyeong />} />
       </Routes>
     </HashRouter>
   );
