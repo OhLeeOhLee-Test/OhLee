a20219e6aa3a70db5bcdec0640330a7579c516dc
YeHwanLee <139995082+YeHwanLee@users.noreply.github.com>
2026-05-04 11:43:52 +0900

    Add files via upload

--- src/pages/home/stageConfig.js
+++ src/pages/home/stageConfig.js
@@ -2,7 +2,7 @@
 
 export const getStageConfig = (device) => {
   return {
-    groundOffset: device === 'mobile' ? '-120vw' : '-200vw',
+    groundOffset: device === 'mobile' ? '-120vw' : '-120vw',
 
     homeText: {
       left: device === 'mobile' ? '10vw' : '15vw',
@@ -55,26 +55,26 @@ export const getStageConfig = (device) => {
     sec1: {
       tree: {
         left: device === 'mobile' ? '10vw' : '10vw',
-        bottom: device === 'mobile' ? '240px' : '12vh', // 190px -> 240px
-        width: device === 'mobile' ? '30vw' : '12vw',
+        bottom: device === 'mobile' ? '240px' : '30vh', // 190px -> 240px
+        width: device === 'mobile' ? '30vw' : '20vw',
         outDir: 'bottom',
       },
       grass1: {
-        left: device === 'mobile' ? '40vw' : '20vw',
-        bottom: device === 'mobile' ? '240px' : '10vh', // 190px -> 240px
-        width: device === 'mobile' ? '20vw' : '8vw',
+        left: device === 'mobile' ? '40vw' : '40vw',
+        bottom: device === 'mobile' ? '240px' : '30vh', // 190px -> 240px
+        width: device === 'mobile' ? '20vw' : '15vw',
         outDir: 'bottom',
       },
       grass1_sub: {
         left: device === 'mobile' ? '30vw' : '30vw',
-        bottom: device === 'mobile' ? '110px' : '2vh', // 60px -> 110px
-        width: device === 'mobile' ? '10vw' : '5vw',
+        bottom: device === 'mobile' ? '110px' : '10vh', // 60px -> 110px
+        width: device === 'mobile' ? '10vw' : '8vw',
         outDir: 'bottom',
       },
       rock: {
-        left: device === 'mobile' ? '80vw' : '40vw',
-        bottom: device === 'mobile' ? '100px' : '1vh', // 50px -> 100px
-        width: device === 'mobile' ? '22vw' : '6vw',
+        left: device === 'mobile' ? '80vw' : '80vw',
+        bottom: device === 'mobile' ? '100px' : '5vh', // 50px -> 100px
+        width: device === 'mobile' ? '22vw' : '16vw',
         outDir: 'bottom',
       },
 
@@ -91,37 +91,37 @@ export const getStageConfig = (device) => {
     // 🌍 2번 무대
     sec2: {
       tree: {
-        left: device === 'mobile' ? '225vw' : '130vw',
-        bottom: device === 'mobile' ? '300px' : '15vh', // 250px -> 300px
-        width: device === 'mobile' ? '40vw' : '15vw',
+        left: device === 'mobile' ? '225vw' : '225vw',
+        bottom: device === 'mobile' ? '300px' : '30vh', // 250px -> 300px
+        width: device === 'mobile' ? '40vw' : '30vw',
         outDir: 'bottom',
       },
       grass2: {
-        left: device === 'mobile' ? '175vw' : '110vw',
-        bottom: device === 'mobile' ? '150px' : '8vh', // 100px -> 150px
-        width: device === 'mobile' ? '13vw' : '6vw',
+        left: device === 'mobile' ? '175vw' : '175vw',
+        bottom: device === 'mobile' ? '150px' : '12vh', // 100px -> 150px
+        width: device === 'mobile' ? '13vw' : '8vw',
         outDir: 'bottom',
       },
       grass2_sub: {
-        left: device === 'mobile' ? '120vw' : '150vw',
-        bottom: device === 'mobile' ? '230px' : '12vh', // 180px -> 230px
-        width: device === 'mobile' ? '25vw' : '8vw',
+        left: device === 'mobile' ? '120vw' : '110vw',
+        bottom: device === 'mobile' ? '230px' : '30vh', // 180px -> 230px
+        width: device === 'mobile' ? '25vw' : '18vw',
         outDir: 'bottom',
       },
       rock: {
-        left: device === 'mobile' ? '210vw' : '170vw',
-        bottom: device === 'mobile' ? '35px' : '2vh', // -15px -> 35px
-        width: device === 'mobile' ? '38vw' : '7vw',
+        left: device === 'mobile' ? '210vw' : '210vw',
+        bottom: device === 'mobile' ? '35px' : '6vh', // -15px -> 35px
+        width: device === 'mobile' ? '38vw' : '25vw',
         outDir: 'bottom',
       },
 
       mailbox: {
-        left: device === 'mobile' ? '195vw' : '180vw',
+        left: device === 'mobile' ? '195vw' : '195vw',
         bottom: device === 'mobile' ? '200px' : '20vh', // 150px -> 200px
         width: device === 'mobile' ? '35vw' : '20vw',
         zIndex: 10,
         outDir: 'bottom',
-        walkToLeft: device === 'mobile' ? '55vw' : '165vw',
+        walkToLeft: device === 'mobile' ? '55vw' : '65vw',
       },
     },
   };
